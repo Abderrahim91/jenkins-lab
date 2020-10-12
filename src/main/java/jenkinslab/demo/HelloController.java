85ca87dd8c2fee5e9b93d393e9b99dcc59cd2565
root <root@localhost.localdomain>
2020-10-12 12:46:46 +0100

    first commit

---
+++ src/main/java/jenkinslab/demo/HelloController.java
@@ -0,0 +1,12 @@
+package jenkinslab.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
---
+++ src/test/java/jenkinslab/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkinslab.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
